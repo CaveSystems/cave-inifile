6bcb9394330345e8f2baad7f34fe389161b0068a
Andreas Rohleder <andreas@rohleder.cc>
2020-04-16 23:44:40 +0200

    Added tests

---
+++ Tests/TestInifile.cs
@@ -0,0 +1,32 @@
+﻿using Cave;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestInifile
+    {
+        [Test]
+        public void Test()
+        {
+            IniWriter writer = new IniWriter();
+            for (int i = 0; i < 100; i++)
+            {
+                var s = TestStruct.Create(i ^ (1 << i % 32));
+                writer.WriteFields($"struct{i}", s);
+            }
+            var reader = writer.ToReader();
+            for (int i = 0; i < 100; i++)
+            {
+                var expected = TestStruct.Create(i ^ (1 << i % 32));
+                var current = reader.ReadStructFields<TestStruct>($"struct{i}");
+                Assert.AreEqual(expected, current);
+            }
+        }
+    }
+}
